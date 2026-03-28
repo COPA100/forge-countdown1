d5c1878fff93d9bd593e20268cbd9b40e9a46280
Colin Park <colinpark100@gmail.com>
2026-03-27 23:35:52 -0400

    more squares

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,27 @@
 import './App.css'
 
-export default function Square() {
-  return <button className="square">X</button>
+export default function Board() {
+  function renderNumberedSquare(n) {
+    return <button className="square">{n}</button>
+  }
+
+  return (
+    <>
+      <div className="board-row">
+        {renderNumberedSquare(1)}
+        {renderNumberedSquare(2)}
+        {renderNumberedSquare(3)}
+      </div>
+      <div className="board-row">
+        {renderNumberedSquare(4)}
+        {renderNumberedSquare(5)}
+        {renderNumberedSquare(6)}
+      </div>
+      <div className="board-row">
+        {renderNumberedSquare(7)}
+        {renderNumberedSquare(8)}
+        {renderNumberedSquare(9)}
+      </div>
+    </>
+  )
 }
